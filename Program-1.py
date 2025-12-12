e9416828b3503bec9e10c1ece0060d15495b6d76
Merlyndeena <merlyndeena@gmail.com>
2025-12-12 17:28:23 +0530

    Create Program-1.py

---
+++ Program-1.py
@@ -0,0 +1,17 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operation: ")
+
+if op == "add":
+    print("Result:", a + b)
+elif op == "sub":
+    print("Result:", a - b)
+elif op == "mul":
+    print("Result:", a * b)
+elif op == "div":
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid operation")
